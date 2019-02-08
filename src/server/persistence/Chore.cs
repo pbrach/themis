592868bc355e532fbb51ff97f4c5ec6abf2f9018
philipp brachvogel <philipp-brachvogel@web.de>
2019-02-08 19:18:19 +0100

    added a basic sqlite database and a corresponding ef context

---
+++ src/server/persistence/Chore.cs
@@ -0,0 +1,9 @@
+namespace persistence
+{
+    public class Chore
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
---
+++ src/server/persistence/ThemisContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace persistence
+{
+    public class ThemisContext : DbContext
+    {
+        public DbSet<Chore> Chores {get; set; }
+        
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlite("Data Source=themis.db");
+        }
+    }
+}
