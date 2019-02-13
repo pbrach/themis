83d687e46aef8355a06b363278660b8defc1c067
philipp brachvogel <philipp-brachvogel@web.de>
2019-02-13 19:18:44 +0100

    added additional properties to the class AppDomain.Chore. They are needed for interval calculation

--- src/Server/AppDomain/Entities/Chore.cs
+++ src/Server/AppDomain/Entities/Chore.cs
@@ -1,8 +1,13 @@
+using System;
+
 namespace AppDomain.Entities
 {
     public class Chore
     {
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string Title { get; set; } = "No Title";
+        public string Description { get; set; } = "";
+        public TimeSpan Duration { get; set; } = TimeSpan.Zero;
+        public DateTime StartTime { get; set; } = DateTime.MinValue;
+        public string[] Users { get; set; } = { };
     }
 }
