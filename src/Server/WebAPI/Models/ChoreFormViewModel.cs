014f97940817acb541a49b4e2712cd415e76ae46
Philipp Brachvogel <philipp-brachvogel@web.de>
2019-02-16 23:37:02 +0100

    added bower.json and .bowerrc to restore wwwroot/lib on checkout

--- src/Server/WebAPI/Models/ChoreFormViewModel.cs
+++ src/Server/WebAPI/Models/ChoreFormViewModel.cs
@@ -7,7 +7,7 @@ namespace WebAPI.Models
     {
         public string Title { get; set; } = "";
         public string Description { get; set; } = "";
-        public TimeSpan Duration { get; set; } = TimeSpan.Zero;
+        public TimeSpan DaysDuration { get; set; } = TimeSpan.Zero;
         public DateTime StartDay { get; set; } = DateTime.Now;
         public string IntervalType { get; set; }
         public ICollection<string> AssignedUsers { get; set; } = new List<string>();
